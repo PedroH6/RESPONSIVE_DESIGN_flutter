7564757d8903928f21a0478e8ed51fc66d7380d7
Pedro Henrique <pedrohenriquebsouza.1@gmail.com>
2025-06-25 22:03:17 -0300

    criando widgets desktop e mobile

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_design/responsive/desktop_body.dart';
+import 'package:responsive_design/responsive/mobile_body.dart';
+import 'package:responsive_design/responsive/responsive.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,9 +14,11 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
    
-
     return Scaffold(
-     
+      body: ResponsiveLayout(
+        mobileBody: MyMobileBody(),
+        desktopBody: MyDesktopBody(),
+      ),
     );
   }
 }
