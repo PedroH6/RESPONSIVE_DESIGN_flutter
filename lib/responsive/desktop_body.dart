f045576437ab3b99b20613f0581160f2e07dd78d
Pedro Henrique <pedrohenriquebsouza.1@gmail.com>
2025-06-25 22:59:31 -0300

    finalizado layout responsivo

--- lib/responsive/desktop_body.dart
+++ lib/responsive/desktop_body.dart
@@ -3,10 +3,54 @@ import 'package:flutter/material.dart';
 class MyDesktopBody extends StatelessWidget {
   const MyDesktopBody({super.key});
 
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green[300],
+      backgroundColor: Colors.deepPurple[200],
+      appBar: AppBar(title: Text('D E S K T O P')),
+      body: Row(
+        children: [
+          // PRIMEIRA COLUMN
+          Expanded(
+            child: Column(
+              children: [
+                // Youtuber video
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: AspectRatio(
+                    aspectRatio: 16 / 9,
+                    child: Container(height: 250, color: Colors.deepPurple[400]),
+                  ),
+                ),
+            
+                //cmment section
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: 8,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(color: Colors.deepPurple[300], height: 120),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          // SEGUNDA COLUMN
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              width: 200,
+              color: Colors.deepPurple[300],
+            
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/responsive/mobile_body.dart
+++ lib/responsive/mobile_body.dart
@@ -8,30 +8,33 @@ class MyMobileBody extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.deepPurple[200],
       appBar: AppBar(title: Text('M O B I L E')),
-      body: Column(
-        children: [
-          // Youtuber video
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: AspectRatio(
-              aspectRatio: 16 / 9,
-              child: Container(height: 250, color: Colors.deepPurple[400]),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            // Youtuber video
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: AspectRatio(
+                aspectRatio: 16 / 9,
+                child: Container(color: Colors.deepPurple[400]),
+              ),
             ),
-          ),
-
-          //cmment section
-          Expanded(
-            child: ListView.builder(
-              itemCount: 8,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(color: Colors.deepPurple[300], height: 120),
-                );
-              },
+        
+            //cmment section
+            Expanded(
+              child: ListView.builder(
+                itemCount: 8,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(color: Colors.deepPurple[300], height: 120),
+                  );
+                },
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
