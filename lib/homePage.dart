588093930ad2a9fb7d9796f8562d0151a19f1543
Pedro Henrique <pedrohenriquebsouza.1@gmail.com>
2025-06-25 21:15:13 -0300

    adicionando layout responsivo

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -10,15 +10,10 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    final currentWidth = MediaQuery.of(context).size.width;
+   
 
     return Scaffold(
-      backgroundColor: currentWidth < 600
-          ? Colors.deepPurple[300]
-          : Colors.green[300],
-      body: Center(
-        child: Text(currentWidth.toString()),
-      ),
+     
     );
   }
 }
